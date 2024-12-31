8a0dbd1fdb0cdd165e13321706997732bf133bf3
Thathshila <thathshilaashanganie9501@gmail.com>
2024-12-31 20:28:00 +0530

    CustomerServlet

--- Application1/src/CustomerServlet.java
+++ Application1/src/CustomerServlet.java
@@ -15,38 +15,38 @@ import java.sql.*;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
-            ResultSet resultSet = connection.prepareStatement("Select * from customer").executeQuery();
-
-            //create json array
-            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
-            while (resultSet.next()) {
-                String id = resultSet.getString("id");
-                String name = resultSet.getString("name");
-                String address = resultSet.getString("address");
-                String tel = resultSet.getString("tel");
-                String email = resultSet.getString("email");
-                // System.out.println(id+" "+name+" "+address+" "+tel+" "+email);
-                JsonObjectBuilder customer = Json.createObjectBuilder();
-                customer.add("id", id);
-                customer.add("name", name);
-                customer.add("address", address);
-                customer.add("tel", tel);
-                customer.add("email", email);
-                allCustomers.add(customer);
-            }
-            resp.setContentType("application/json");
-            resp.getWriter().write(allCustomers.build().toString());
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
+//            ResultSet resultSet = connection.prepareStatement("Select * from customer").executeQuery();
+//
+//            //create json array
+//            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+//            while (resultSet.next()) {
+//                String id = resultSet.getString("id");
+//                String name = resultSet.getString("name");
+//                String address = resultSet.getString("address");
+//                String tel = resultSet.getString("tel");
+//                String email = resultSet.getString("email");
+//                // System.out.println(id+" "+name+" "+address+" "+tel+" "+email);
+//                JsonObjectBuilder customer = Json.createObjectBuilder();
+//                customer.add("id", id);
+//                customer.add("name", name);
+//                customer.add("address", address);
+//                customer.add("tel", tel);
+//                customer.add("email", email);
+//                allCustomers.add(customer);
+//            }
+//            resp.setContentType("application/json");
+//            resp.getWriter().write(allCustomers.build().toString());
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -170,6 +170,112 @@ public class CustomerServlet extends HttpServlet {
                 }
             }
         } catch (SQLException e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write("{\"error\": \"An internal error occurred.\"}");
+        }
+    }
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
+//
+//            String telOnly = req.getParameter("telOnly");
+//            String query = telOnly != null && telOnly.equals("true")
+//                    ? "SELECT tel FROM customer"
+//                    : "SELECT * FROM customer";
+//
+//            ResultSet resultSet = connection.prepareStatement(query).executeQuery();
+//            resp.setContentType("application/json");
+//
+//            if (telOnly != null && telOnly.equals("true")) {
+//                // Create a JSON array for telephone numbers
+//                JsonArrayBuilder telArray = Json.createArrayBuilder();
+//                while (resultSet.next()) {
+//                    telArray.add(resultSet.getString("tel"));
+//                }
+//                resp.getWriter().write(telArray.build().toString());
+//            } else {
+//                // Existing logic for all customer details
+//                JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+//                while (resultSet.next()) {
+//                    JsonObjectBuilder customer = Json.createObjectBuilder();
+//                    customer.add("id", resultSet.getString("id"));
+//                    customer.add("name", resultSet.getString("name"));
+//                    customer.add("address", resultSet.getString("address"));
+//                    customer.add("tel", resultSet.getString("tel"));
+//                    customer.add("email", resultSet.getString("email"));
+//                    allCustomers.add(customer);
+//                }
+//                resp.getWriter().write(allCustomers.build().toString());
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            resp.getWriter().write("{\"error\": \"An internal error occurred.\"}");
+//        }
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
+
+            String telOnly = req.getParameter("telOnly");
+            String telParam = req.getParameter("tel");
+
+            String query;
+            PreparedStatement preparedStatement;
+
+            if (telOnly != null && telOnly.equals("true")) {
+                query = "SELECT tel FROM customer";
+                preparedStatement = connection.prepareStatement(query);
+            } else if (telParam != null) {
+                query = "SELECT id FROM customer WHERE tel = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, telParam);
+            } else {
+                query = "SELECT * FROM customer";
+                preparedStatement = connection.prepareStatement(query);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resp.setContentType("application/json");
+
+            if (telOnly != null && telOnly.equals("true")) {
+
+                JsonArrayBuilder telArray = Json.createArrayBuilder();
+                while (resultSet.next()) {
+                    telArray.add(resultSet.getString("tel"));
+                }
+                resp.getWriter().write(telArray.build().toString());
+            } else if (telParam != null) {
+
+                JsonObjectBuilder customerIdJson = Json.createObjectBuilder();
+                if (resultSet.next()) {
+                    customerIdJson.add("id", resultSet.getString("id"));
+                } else {
+                    customerIdJson.add("id", "");
+                }
+                resp.getWriter().write(customerIdJson.build().toString());
+            } else {
+                JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+                while (resultSet.next()) {
+                    JsonObjectBuilder customer = Json.createObjectBuilder();
+                    customer.add("id", resultSet.getString("id"));
+                    customer.add("name", resultSet.getString("name"));
+                    customer.add("address", resultSet.getString("address"));
+                    customer.add("tel", resultSet.getString("tel"));
+                    customer.add("email", resultSet.getString("email"));
+                    allCustomers.add(customer);
+                }
+                resp.getWriter().write(allCustomers.build().toString());
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.getWriter().write("{\"error\": \"An internal error occurred.\"}");
